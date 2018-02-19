084a228fc4e8f4c2fc85650b116def7bfc671a4b
ipeter50 <pierre.le-jeune@live.fr>
2018-02-19 09:51:21 +0100

    Test NN

--- testRandomForest.py
+++ testRandomForest.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
 import numpy
 import matplotlib.pyplot as plt
 
@@ -35,28 +36,39 @@ def validation(clf,X,y):
 (names1,X1,y1)=importData("D:\\Documents\\Centrale\\Ei2\\PGROU\\data.csv")
 (names2,X2,y2)=importData("D:\\Documents\\Centrale\\Ei2\\PGROU\\test.csv")
 
-print(names1)
-print(X1)
-print(y1)
 
 
 p1=[]
 p2=[]
 x=numpy.array([i for i in range(1,10)])
+clf1 = RandomForestClassifier(n_estimators=100,criterion='entropy',max_features='log2')
+clf1.fit(X1, y1)
+clf2 = RandomForestClassifier(n_estimators=100,criterion='entropy')
+clf2.fit(X2, y2)
+j=4
+for i in range(2,30):
+    
+    
+    
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(i, j), random_state=1)
+    clf.fit(X1,y1)
+    clf1 = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(i, j), random_state=1)
+    clf1.fit(X1,y1)
 
+    p1.append(validation(clf,X2,y2))
+    p2.append(validation(clf1,X1,y1))
 
-for i in range(90,100):
-    clf1 = RandomForestClassifier(n_estimators=i,criterion='entropy',max_features='log2')
-    clf1.fit(X1, y1)
-    clf2 = RandomForestClassifier(n_estimators=i,criterion='entropy')
-    clf2.fit(X2, y2)
-    p1.append(validation(clf1,X2,y2))
-    p2.append(validation(clf2,X1,y1))
 
 plt.plot(p1)
-print(numpy.mean(p1))
 plt.plot(p2)
+print(numpy.mean(p1))
+print(max(p1))
+print(min(p1))
+print("\n")
 print(numpy.mean(p2))
+print(max(p2))
+print(min(p2))
+    
 plt.show()
 
 
